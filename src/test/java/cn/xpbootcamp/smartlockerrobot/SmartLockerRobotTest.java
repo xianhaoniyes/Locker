de435f21dca4104dc9710c9e8e35e0d500be0c68
xianhaoni <xianhaoni@gmail.com>
2020-09-05 13:20:44 +0800

    [xianhao] should_save_in_1st_locker_and_return_ticket_when_1st_and_2st_has_the_same_capacity

--- src/test/java/cn/xpbootcamp/smartlockerrobot/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/smartlockerrobot/SmartLockerRobotTest.java
@@ -32,4 +32,20 @@ public class SmartLockerRobotTest {
         Assert.assertNotNull(ticket);
         Assert.assertSame(bag, secondLocker.pickupBy(ticket));
     }
+
+    @Test
+    public void should_save_in_1st_locker_and_return_ticket_when_1st_and_2st_has_the_same_capacity(){
+
+        Locker firstLocker = new Locker(5);
+        Locker secondLocker = new Locker(5);
+        SmartLockerRobot robot = new SmartLockerRobot(Arrays.asList(firstLocker,secondLocker));
+        Bag  bag = new Bag();
+
+        Ticket ticket = robot.save(bag);
+        Assert.assertNotNull(ticket);
+        Assert.assertSame(bag, firstLocker.pickupBy(ticket));
+    }
+
+
+
 }
