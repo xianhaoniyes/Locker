adbcfb5526df8da9f5ae72ca84e474e25ff95e43
xianhaoni <xianhaoni@gmail.com>
2020-09-05 10:54:11 +0800

    [xianhao] locker could save bag and return ticket when has capacity

---
+++ src/test/java/cn/xpbootcamp/smartprimaryrobot/Locker.java
@@ -0,0 +1,11 @@
+package cn.xpbootcamp.smartprimaryrobot;
+
+public class Locker {
+    public Locker(int capacity) {
+    }
+
+    public Ticket save(Bag bag) {
+
+        return new Ticket();
+    }
+}
---
+++ src/test/java/cn/xpbootcamp/smartprimaryrobot/LockerTest.java
@@ -0,0 +1,14 @@
+package cn.xpbootcamp.smartprimaryrobot;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class LockerTest {
+
+    @Test
+    public void should_return_ticket_when_locker_save_bag_given_locker_has_available_capacity(){
+        Locker locker = new Locker(10);
+        Ticket ticket = locker.save(new Bag());
+        Assert.assertNotNull(ticket);
+    }
+}
